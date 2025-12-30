1c0e1f3de74909c452cce60e4718c2cdbf078f0c
Gilson Joanelo <gilsonjoanelo@gmail.com>
2025-12-29 22:59:59 -0300

    ajuste Check-ExitCode

--- include.ps1
+++ include.ps1
@@ -1,7 +1,9 @@
 function Check-ExitCode() {
 	$exitCode = $LASTEXITCODE
-	if ($exitCode -ne 0) {
-		echo "Non-zero ($exitCode) exit code. Exiting..."
-		exit $exitCode
-	}
+	    if ($exitCode -ne 0) {
+        Write-Error "❌ Non-zero exit code detected: $exitCode"
+        exit $exitCode
+    } else {
+        Write-Host "✅ Exit code $exitCode (success)"
+    }
 }
