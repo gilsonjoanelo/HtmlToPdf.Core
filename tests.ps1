8d57aec7c72beba9f9e7232648414d5e30741a37
Gilson Joanelo <gilsonjoanelo@gmail.com>
2025-12-29 22:58:08 -0300

    ajuste test

--- tests.ps1
+++ tests.ps1
@@ -14,20 +14,20 @@ $frameworkVersion = 'net9.0'
 function Prepare() {
 	Write-Host "=== $($MyInvocation.MyCommand.Name) ==="
     pushd $baseDir
-    Write-Host "=== END ==="
+    #Write-Host "=== END ==="
 }
 
 function Cleanup() {
 	Write-Host "=== $($MyInvocation.MyCommand.Name) ==="
     popd
-    Write-Host "=== END ==="
+    #Write-Host "=== END ==="
 }
 
 function Tests-All() {
 	Write-Host "=== Running all tests ==="
     dotnet test "$baseDir\src\HtmlToPdf.Core.slnx" -c $Configuration --framework $frameworkVersion --logger "trx;LogFileName=TestResults.trx"
     Check-ExitCode
-    Write-Host "=== Tests completed ==="
+    #Write-Host "=== Tests completed ==="
 }
 
 try {
